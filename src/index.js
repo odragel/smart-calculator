25d0638ed0f87cf24126c2c821d49af4d8f60ae0
odragel <olga.dragel@gmail.com>
2018-02-28 01:00:22 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,66 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        // your implementation
+        this.initialValue = initialValue;
+        this.elements =[];
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        // your implementation
+        this.elements.push({operand: "+", curNumber: number});
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        // your implementation
+        this.elements.push({operand: "-", curNumber: number});
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        // your implementation
+        this.elements.push({operand: "*", curNumber: number});
+        return this;
+    }
+
+    devide(number) {
+        // your implementation
+        this.elements.push({operand: "/", curNumber: number});
+        return this;
+    }
+
+    pow(number) {
+        // your implementation
+        this.elements.push({operand: "pow", curNumber: number});
+        return this;
+        }
+
+    toString() {
+        var result="";
+        while(this.elements.length > 0){
+          var curEl =this.elements.pop();
+          if(curEl.operand != "pow"){
+            result = curEl.operand + curEl.curNumber + result;
+          }else{
+            if(this.elements.length > 0){
+                this.elements[this.elements.length - 1].curNumber = "Math.pow("+this.elements[this.elements.length - 1].curNumber+","+curEl.curNumber+")";
+            } else{
+                result = "Math.pow("+this.initialValue+","+curEl.curNumber+")" + result;
+                this.initialValue = null;
+            }
+          }
+        }
+        if(this.initialValue!= null){
+            result=this.initialValue+result;
+        }
+
+
+        return eval(result);
+
+
+    }
+
 }
 
 module.exports = SmartCalculator;
